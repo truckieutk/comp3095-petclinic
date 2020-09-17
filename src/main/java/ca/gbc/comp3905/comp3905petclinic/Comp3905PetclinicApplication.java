ad916eecc97213ace4e784505c217b2b246c0b4c
truckieutk <jasminetruckieu@gmail.com>
2020-09-17 16:47:30 -0400

    This is pet clinic project for lab 2

---
+++ src/main/java/ca/gbc/comp3905/comp3905petclinic/Comp3905PetclinicApplication.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3905.comp3905petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp3905PetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Comp3905PetclinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/gbc/comp3905/comp3905petclinic/Comp3905PetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3905.comp3905petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp3905PetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
